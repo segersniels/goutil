1ba18353322db17767fc79d5dd4c7560e8c6ab82
Niels Segers <segers.n@hotmail.com>
2018-04-19 11:31:06 +0200

    add error logging and exit code 0

--- logging.go
+++ logging.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"os"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
@@ -15,6 +16,12 @@ func Log(a ...interface{}) {
 // Warn : custom TTY warning logging
 func Warn(a ...interface{}) {
 	logrus.Warn(a...)
+}
+
+// Error : custom TTY error logging and throw exit code 0
+func Error(a ...interface{}) {
+	logrus.Error(a...)
+	os.Exit(0)
 }
 
 // Fatal : custom TTY fatal logging
@@ -38,6 +45,12 @@ func LogWithFields(fields map[string]interface{}, value string) {
 // WarnWithFields : custom warning with fields
 func WarnWithFields(fields map[string]interface{}, value string) {
 	logrus.WithFields(fields).Warn(value)
+}
+
+// ErrorWithFields : custom error with fields and exit code 0
+func ErrorWithFields(fields map[string]interface{}, value string) {
+	logrus.WithFields(fields).Error(value)
+	os.Exit(0)
 }
 
 // FatalWithFields : custom panic with fields
